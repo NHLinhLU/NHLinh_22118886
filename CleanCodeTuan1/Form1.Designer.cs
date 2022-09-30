cc4268b5f1f86e8c017b9573300eb75223c72ea6
NHLinhLU <114724546+NHLinhLU@users.noreply.github.com>
2022-09-30 14:30:27 +0700

    Add files via upload

---
+++ CleanCodeTuan1/Form1.Designer.cs
@@ -0,0 +1,387 @@
+﻿namespace CleanCodeTuan1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fnl_top = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_stgs = new System.Windows.Forms.Button();
+            this.btn_hp = new System.Windows.Forms.Button();
+            this.panel_centre = new System.Windows.Forms.Panel();
+            this.pnl_room_2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pnl_room_1 = new System.Windows.Forms.Panel();
+            this.lbl_1_off = new System.Windows.Forms.Label();
+            this.lbl_1_on = new System.Windows.Forms.Label();
+            this.lbl_light_1 = new System.Windows.Forms.Label();
+            this.pnl_bot = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.fnl_top.SuspendLayout();
+            this.panel_centre.SuspendLayout();
+            this.pnl_room_2.SuspendLayout();
+            this.pnl_room_1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fnl_top
+            // 
+            this.fnl_top.BackColor = System.Drawing.Color.Pink;
+            this.fnl_top.Controls.Add(this.btn_1);
+            this.fnl_top.Controls.Add(this.btn_stgs);
+            this.fnl_top.Controls.Add(this.btn_hp);
+            this.fnl_top.Dock = System.Windows.Forms.DockStyle.Top;
+            this.fnl_top.Location = new System.Drawing.Point(0, 0);
+            this.fnl_top.Name = "fnl_top";
+            this.fnl_top.Size = new System.Drawing.Size(742, 100);
+            this.fnl_top.TabIndex = 0;
+            // 
+            // btn_1
+            // 
+            this.btn_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_1.Location = new System.Drawing.Point(3, 3);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(230, 91);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.Text = "Dashboard";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_stgs
+            // 
+            this.btn_stgs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_stgs.Location = new System.Drawing.Point(239, 3);
+            this.btn_stgs.Name = "btn_stgs";
+            this.btn_stgs.Size = new System.Drawing.Size(248, 91);
+            this.btn_stgs.TabIndex = 1;
+            this.btn_stgs.Text = "Settings";
+            this.btn_stgs.UseVisualStyleBackColor = true;
+            this.btn_stgs.Click += new System.EventHandler(this.btn_stgs_Click);
+            // 
+            // btn_hp
+            // 
+            this.btn_hp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_hp.Location = new System.Drawing.Point(493, 3);
+            this.btn_hp.Name = "btn_hp";
+            this.btn_hp.Size = new System.Drawing.Size(241, 91);
+            this.btn_hp.TabIndex = 2;
+            this.btn_hp.Text = "Help";
+            this.btn_hp.UseVisualStyleBackColor = true;
+            this.btn_hp.Click += new System.EventHandler(this.btn_hp_Click);
+            // 
+            // panel_centre
+            // 
+            this.panel_centre.BackColor = System.Drawing.Color.Black;
+            this.panel_centre.Controls.Add(this.panel1);
+            this.panel_centre.Controls.Add(this.pnl_room_2);
+            this.panel_centre.Controls.Add(this.pnl_room_1);
+            this.panel_centre.Controls.Add(this.pnl_bot);
+            this.panel_centre.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_centre.Location = new System.Drawing.Point(0, 100);
+            this.panel_centre.Name = "panel_centre";
+            this.panel_centre.Size = new System.Drawing.Size(742, 350);
+            this.panel_centre.TabIndex = 1;
+            // 
+            // pnl_room_2
+            // 
+            this.pnl_room_2.BackColor = System.Drawing.Color.Yellow;
+            this.pnl_room_2.Controls.Add(this.button8);
+            this.pnl_room_2.Controls.Add(this.button5);
+            this.pnl_room_2.Controls.Add(this.button1);
+            this.pnl_room_2.Controls.Add(this.label2);
+            this.pnl_room_2.Controls.Add(this.label4);
+            this.pnl_room_2.Location = new System.Drawing.Point(239, 6);
+            this.pnl_room_2.Name = "pnl_room_2";
+            this.pnl_room_2.Size = new System.Drawing.Size(230, 154);
+            this.pnl_room_2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(135, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "OFF";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.LavenderBlush;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(86, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ON";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // pnl_room_1
+            // 
+            this.pnl_room_1.BackColor = System.Drawing.Color.Red;
+            this.pnl_room_1.Controls.Add(this.button7);
+            this.pnl_room_1.Controls.Add(this.button4);
+            this.pnl_room_1.Controls.Add(this.button2);
+            this.pnl_room_1.Controls.Add(this.lbl_1_off);
+            this.pnl_room_1.Controls.Add(this.lbl_1_on);
+            this.pnl_room_1.Controls.Add(this.lbl_light_1);
+            this.pnl_room_1.Location = new System.Drawing.Point(3, 6);
+            this.pnl_room_1.Name = "pnl_room_1";
+            this.pnl_room_1.Size = new System.Drawing.Size(230, 154);
+            this.pnl_room_1.TabIndex = 1;
+            this.pnl_room_1.Paint += new System.Windows.Forms.PaintEventHandler(this.pnl_room1_Paint);
+            // 
+            // lbl_1_off
+            // 
+            this.lbl_1_off.AutoSize = true;
+            this.lbl_1_off.BackColor = System.Drawing.Color.Black;
+            this.lbl_1_off.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_1_off.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_1_off.Location = new System.Drawing.Point(141, 9);
+            this.lbl_1_off.Name = "lbl_1_off";
+            this.lbl_1_off.Size = new System.Drawing.Size(41, 18);
+            this.lbl_1_off.TabIndex = 5;
+            this.lbl_1_off.Text = "OFF";
+            this.lbl_1_off.Click += new System.EventHandler(this.lbl_1_off_Click);
+            // 
+            // lbl_1_on
+            // 
+            this.lbl_1_on.AutoSize = true;
+            this.lbl_1_on.BackColor = System.Drawing.Color.LavenderBlush;
+            this.lbl_1_on.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_1_on.Location = new System.Drawing.Point(92, 9);
+            this.lbl_1_on.Name = "lbl_1_on";
+            this.lbl_1_on.Size = new System.Drawing.Size(33, 18);
+            this.lbl_1_on.TabIndex = 3;
+            this.lbl_1_on.Text = "ON";
+            this.lbl_1_on.Click += new System.EventHandler(this.lbl_1_on_Click);
+            // 
+            // lbl_light_1
+            // 
+            this.lbl_light_1.AutoSize = true;
+            this.lbl_light_1.BackColor = System.Drawing.Color.Pink;
+            this.lbl_light_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_light_1.Location = new System.Drawing.Point(15, 9);
+            this.lbl_light_1.Name = "lbl_light_1";
+            this.lbl_light_1.Size = new System.Drawing.Size(0, 18);
+            this.lbl_light_1.TabIndex = 2;
+            this.lbl_light_1.Click += new System.EventHandler(this.lbl_light_1_Click);
+            // 
+            // pnl_bot
+            // 
+            this.pnl_bot.BackColor = System.Drawing.Color.Pink;
+            this.pnl_bot.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnl_bot.Location = new System.Drawing.Point(0, 242);
+            this.pnl_bot.Name = "pnl_bot";
+            this.pnl_bot.Size = new System.Drawing.Size(742, 108);
+            this.pnl_bot.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Lime;
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label17);
+            this.panel1.Controls.Add(this.label19);
+            this.panel1.Location = new System.Drawing.Point(475, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(230, 154);
+            this.panel1.TabIndex = 13;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.BackColor = System.Drawing.Color.Black;
+            this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label17.Location = new System.Drawing.Point(135, 9);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(41, 18);
+            this.label17.TabIndex = 5;
+            this.label17.Text = "OFF";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.BackColor = System.Drawing.Color.LavenderBlush;
+            this.label19.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label19.Location = new System.Drawing.Point(86, 9);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(33, 18);
+            this.label19.TabIndex = 3;
+            this.label19.Text = "ON";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "light";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "light";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(18, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "light";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(18, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(63, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "+5S";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(17, 70);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(63, 23);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "+5S";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(18, 70);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(63, 23);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "+5S";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(18, 119);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(63, 23);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "-5S";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(17, 119);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(63, 23);
+            this.button8.TabIndex = 18;
+            this.button8.Text = "-5S";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(18, 119);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(63, 23);
+            this.button9.TabIndex = 19;
+            this.button9.Text = "-5S";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 450);
+            this.Controls.Add(this.panel_centre);
+            this.Controls.Add(this.fnl_top);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.fnl_top.ResumeLayout(false);
+            this.panel_centre.ResumeLayout(false);
+            this.pnl_room_2.ResumeLayout(false);
+            this.pnl_room_2.PerformLayout();
+            this.pnl_room_1.ResumeLayout(false);
+            this.pnl_room_1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel fnl_top;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_stgs;
+        private System.Windows.Forms.Button btn_hp;
+        private System.Windows.Forms.Panel panel_centre;
+        private System.Windows.Forms.Panel pnl_room_1;
+        private System.Windows.Forms.Label lbl_light_1;
+        private System.Windows.Forms.Panel pnl_bot;
+        private System.Windows.Forms.Label lbl_1_off;
+        private System.Windows.Forms.Label lbl_1_on;
+        private System.Windows.Forms.Panel pnl_room_2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CleanCodeTuan1/FrmRoom.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CleanCodeTuan1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            port = new SerialPort(COM1)
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnl_top_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_stgs_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_hp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnl_room1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_light_1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_1_off_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_2_off_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_1_on_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_light_2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_2_on_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lb_name_room1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
